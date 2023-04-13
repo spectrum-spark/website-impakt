94299ddca89499d5710d4556bd26a945657a2797
Michael Lydeamore <michael.lydeamore@monash.edu>
2023-04-13 16:59:35 +1000

    Fix key collaborators & update images

--- spectrum/copy_images.R
+++ spectrum/copy_images.R
@@ -1,5 +1,7 @@
 library(googledrive)
 
+source("spectrum/load_data.R")
+
 drive <- shared_drive_find("SPECTRUM-SPARK Website")
 
 staff_pictures <- drive %>%
@@ -10,12 +12,20 @@ staff_pictures <- drive %>%
     pull(id) %>%
     drive_ls()
 
+spectrum_staff_ids <- staff_csv |> pull(id)
+
 # Clean out directory
 unlink(list.files(here::here("spectrum", "images", "staff"), full.names = T))
 for (i in 1:nrow(staff_pictures)) {
-    drive_download(
-        staff_pictures[i, "id"] %>% pull(),
-        path = here::here("spectrum", "images", "staff", staff_pictures[i, "name"] %>% pull()),
-        overwrite = T
-    )
+    picture_id <- staff_pictures[i, "name"] |>
+        pull() |>
+        tools::file_path_sans_ext()
+
+    if (picture_id %in% spectrum_staff_ids) {
+        drive_download(
+            staff_pictures[i, "id"] %>% pull(),
+            path = here::here("spectrum", "images", "staff", staff_pictures[i, "name"] %>% pull()),
+            overwrite = T
+        )
+    }
 }
